eb3e678077e4b80619a57d549229b9f02d0445f6
Matheus <matheus-holanda@hotmail.com>
2018-07-28 12:50:12 -0300

    modificando a main

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -2,5 +2,9 @@ public class helloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("dasdas");
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("ola"); 
+        }
     }
 }
